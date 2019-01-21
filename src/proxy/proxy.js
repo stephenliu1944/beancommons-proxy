c2b9d362c67dc29c95a6bd50095fb778bdc63422
liujunjie <liujunjie@bbdservice.com>
2019-01-21 16:59:16 +0800

    增加response set-cookie的拦截, 针对https

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -87,6 +87,17 @@ function setProxyOptions(options = {}, defaults = {}) {
     return proxyOptions;
 }
 
+export function onProxyRes(proxyRes, req, res) {
+    var cookies = proxyRes.headers['set-cookie'];  
+    if (cookies && cookies.length > 0) {
+        var cookiesStr = JSON.stringify(cookies);
+        var localSetCookie = cookiesStr.replace(/(;\s*Domain=[\w|-|\.]+)|(Domain=[\w|-|\.]+;\s*)/g, '')
+            .replace(/(;\s*HttpOnly)|(HttpOnly;\s*)/, '')
+            .replace(/(;\s*Secure)|(Secure;\s*)/, '');
+        proxyRes.headers['set-cookie'] = JSON.parse(localSetCookie);
+    }
+}
+
 export function proxy(services, defaultOpts = {}) {
     var { prefix = 'proxy', ...other } = defaultOpts;
     var matchings = setPathsMatching(services, prefix);
